ad6c91e397890d37b74c5844db47821728d8369c
zeitzn <smevy4lc3@gmail.com>
2019-12-08 00:11:47 -0500

    emitir boleta

--- apps/recibo/urls.py
+++ apps/recibo/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth.decorators import login_required
 app_name='recibo'
 urlpatterns = [    
     path('factura/<mesa>/<cliente>/<ruc>/<direccion>/<total_letras>', views.generarFactura, name='generarFactura'),   
+    path('boleta/<mesa>/<cliente>/<ruc>/<direccion>', views.generarBoleta, name='generarBoleta'),   
 ]
--- apps/recibo/views.py
+++ apps/recibo/views.py
@@ -34,11 +34,15 @@ def generarFactura(request, mesa='', cliente='', ruc='', direccion='', total_let
         month=fecha.month
         year=fecha.year
 
+        print(len(result))
+        print(result[0][0])
+
         if len(result)>0:
-            if result[0][0]!='':
-                ultimo_numero_factura=result[0][0]
+            # if result[0][0]!='':
+            if result[0][0] is None:
+                ultimo_numero_factura=0                
             else:
-                ultimo_numero_factura=0
+                ultimo_numero_factura=result[0][0]
         else:
             ultimo_numero_factura=0
         # print(ultimo_numero_factura[0][0])
@@ -81,9 +85,6 @@ def generarFactura(request, mesa='', cliente='', ruc='', direccion='', total_let
             oPedido.fecha=fecha
             oPedido.save()
 
-        # diccionario_pedidos['pedidos']=lista_pedidos
-        #4
-
     oCliente=Cliente.objects.filter(numero=ruc)
 
     if len(oCliente)<1:
@@ -97,8 +98,6 @@ def generarFactura(request, mesa='', cliente='', ruc='', direccion='', total_let
         
     igv=total*0.18
     context={
-        # 'titulo':titulo,
-        # 'licencias':licencias,
         'logo':logo,
         'pedidos':lista_pedidos,
         'total':total,
@@ -113,15 +112,107 @@ def generarFactura(request, mesa='', cliente='', ruc='', direccion='', total_let
         'anio':year,
         'total_letras':total_letras
     }
-
-    # if forma=='1':
-    #     html = render_to_string("report/report_general_pdf.html",context)
-    # else:
     html = render_to_string("recibo/factura.html",context)
     font_config = FontConfiguration()
     HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
     return response
 
+def generarBoleta(request, mesa='', cliente='', ruc='', direccion=''):
+
+    response = HttpResponse(content_type="application/pdf")
+    response['Content-Disposition'] = "inline; filename=date-name-donation-receipt.pdf"
+  
+    logo = os.path.join(settings.BASE_DIR, 'static/assets/images/logo.png')
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT COUNT(producto_id), ped.mesa, prod.nombre, prod.precio,ped.id AS pedido_id, prod.id AS producto_id FROM detalle_pedido det inner join producto prod ON prod.id=det.producto_id INNER JOIN pedido ped ON det.pedido_id=ped.id WHERE (ped.mesa='+mesa+' AND ped.activo=1) GROUP BY det.producto_id, ped.id ORDER BY ped.id and prod.id;')
+        pedidos=cursor.fetchall()
+
+        cursor.execute('SELECT MAX(numero) FROM pedido WHERE tipo="boleta"')
+        result=cursor.fetchall()
+        fecha = date.today()
+
+        day=fecha.day
+        month=fecha.month
+        year=fecha.year
+
+
+        if len(result)>0:            
+            if result[0][0] is None:
+                ultimo_numero_boleta=0                
+            else:
+                ultimo_numero_boleta=result[0][0]
+        else:
+            ultimo_numero_boleta=0
+        # print(ultimo_numero_boleta[0][0])
+
+        numero_boleta=int(ultimo_numero_boleta)+1
+        numero_boleta_vista=''
+
+        if(numero_boleta<10):
+            numero_boleta_vista='000000'+str(numero_boleta)
+        elif(numero_boleta>10 and numero_boleta<100):
+            numero_boleta_vista='00000'+str(numero_boleta)
+        elif(numero_boleta>100 and numero_boleta<1000):
+            numero_boleta_vista='0000'+str(numero_boleta)
+        elif(numero_boleta>1000 and numero_boleta<10000):
+            numero_boleta_vista='000'+str(numero_boleta)
+        elif(numero_boleta>10000 and numero_boleta<100000):
+            numero_boleta_vista='00'+str(numero_boleta)
+        elif(numero_boleta>100000 and numero_boleta<1000000):
+            numero_boleta_vista='0'+str(numero_boleta)
+        else:
+            numero_boleta_vista=str(numero_boleta)
+        
+        diccionario_pedidos = {}
+        lista_pedidos=[]
+        total=0
+        for row in pedidos:
+            print(row)
+            cantidad=int(row[0])
+            nombre=row[2]
+            precio=float(row[3])
+            valor=cantidad*precio
+            total+=valor
+            pedido={'cantidad':cantidad,'nombre':nombre,'precio':precio,'valor':valor}
+            lista_pedidos.append(pedido)
+
+            oPedido=Pedido.objects.get(id=row[4])
+            oPedido.numero=numero_boleta
+            oPedido.tipo='boleta'
+            oPedido.activo=False
+            oPedido.fecha=fecha
+            oPedido.save()
+
+    oCliente=Cliente.objects.filter(numero=ruc)
+
+    if len(oCliente)<1:
+        oCliente=Cliente(
+            nombres=cliente,
+            numero=ruc,
+            direccion=direccion,
+            tipo='dni'
+        )
+        oCliente.save()
+        
+    igv=total*0.18
+    context={
+        'logo':logo,
+        'pedidos':lista_pedidos,
+        'total':total,
+        'igv':igv,
+        'subtotal':total-igv,
+        'cliente':cliente,
+        'ruc':ruc,
+        'direccion':direccion,
+        'numero_boleta':numero_boleta_vista,
+        'dia':day,
+        'mes':getMonth(month),
+        'anio':year,
+    }
+    html = render_to_string("recibo/boleta.html",context)
+    font_config = FontConfiguration()
+    HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
+    return response
 
 def getMonth(data):
     if data==1:
